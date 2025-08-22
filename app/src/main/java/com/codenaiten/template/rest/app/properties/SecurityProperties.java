da18ab9fb270094dcaace7955a14d8dbf9819184
Ángel Herce Soto <indenaiten@gmail.com>
2025-08-22 17:16:01 +0200

    feat(security): SpringBoot Security Config
    - Se ha añadido la dependecia org.springframework.boot:spring-boot-starter-security al módulo boot.
    
    - Se han establecido las propiedades de seguridad de la aplicación en el fichero application.yaml.
    
    - Se ha creado la clase genérica Properties para aportar lógica común a todas las clases que representen las propiedades de la aplicación.
    
    - Se ha creado la clase de propiedades Security Properties para obtener las propiedades configuradas en el fichero application.yaml.
    
    - Se ha establecido la configuración de la seguridad a partir de las propiedades de seguridad de las aplicación.

---
+++ app/src/main/java/com/codenaiten/template/rest/app/properties/SecurityProperties.java
@@ -0,0 +1,62 @@
+package com.codenaiten.template.rest.app.properties;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Getter
+@Component
+public class SecurityProperties extends Properties {
+
+    @Value( "${app.security.ignore-paths:}" )
+    private String ignorePaths;
+
+    @Value( "${app.security.cors.allowed-origins:}" )
+    private String corsAllowedOrigins;
+
+    @Value( "${app.security.cors.allowed-methods:}" )
+    private String corsAllowedMethods;
+
+    @Value( "${app.security.cors.allowed-headers:}" )
+    private String corsAllowedHeaders;
+
+    @Value( "${app.security.cors.exposed-headers:}" )
+    private String corsExposedHeaders;
+
+    @Value( "${app.security.cors.allow-credentials:}" )
+    private Boolean corsAllowCredentials;
+
+// ------------------------------------------------------------------------------------------------------------------ \\
+// ---| GETTERS |---------------------------------------------------------------------------------------------------- \\
+// ------------------------------------------------------------------------------------------------------------------ \\
+
+    public List<String> getIgnorePaths() {
+        return this.convertToList( this.ignorePaths );
+    }
+
+    public List<String> getCorsAllowedOrigins() {
+        return this.convertToList( this.corsAllowedOrigins );
+    }
+
+    public List<String> getCorsAllowedMethods() {
+        return this.convertToList( this.corsAllowedMethods );
+    }
+
+    public List<String> getCorsAllowedHeaders() {
+        return this.convertToList( this.corsAllowedHeaders );
+    }
+
+    public List<String> getCorsExposedHeaders() {
+        return this.convertToList( this.corsExposedHeaders );
+    }
+
+    public Optional<Boolean> getCorsAllowCredentials() {
+        return this.getOptional( this.corsAllowCredentials );
+    }
+
+// ------------------------------------------------------------------------------------------------------------------ \\
+
+}
---
+++ boot/src/main/java/com/codenaiten/template/rest/boot/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package com.codenaiten.template.rest.boot.config;
+
+import com.codenaiten.template.rest.app.properties.SecurityProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig{
+
+    private final SecurityProperties securityProperties;
+
+// ------------------------------------------------------------------------------------------------------------------ \\
+// ---| BEANS |------------------------------------------------------------------------------------------------------ \\
+// ------------------------------------------------------------------------------------------------------------------ \\
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration cfg = new CorsConfiguration();
+
+        cfg.setAllowedOrigins( this.securityProperties.getCorsAllowedOrigins());
+        if( this.securityProperties.getCorsAllowedOrigins().contains( "*" ))
+            cfg.setAllowedOriginPatterns( this.securityProperties.getCorsAllowedOrigins() );
+
+        cfg.setAllowedMethods( this.securityProperties.getCorsAllowedMethods() );
+        cfg.setAllowedHeaders( this.securityProperties.getCorsAllowedHeaders() );
+        cfg.setExposedHeaders( this.securityProperties.getCorsExposedHeaders() );
+
+        this.securityProperties.getCorsAllowCredentials().ifPresent( cfg::setAllowCredentials );
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration( "/**", cfg );
+        return source;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain( final HttpSecurity http, final CorsConfigurationSource corsConfigurationSource ) throws Exception {
+        http.csrf( CsrfConfigurer::disable )
+                .cors(cors -> cors.configurationSource( corsConfigurationSource ))
+                .sessionManagement(sess -> sess.sessionCreationPolicy( SessionCreationPolicy.STATELESS ))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers( this.securityProperties.getIgnorePaths().toArray( new String[0] )).permitAll()
+                        .anyRequest().authenticated());
+        return http.build();
+    }
+
+// ------------------------------------------------------------------------------------------------------------------ \\
+
+}
+
