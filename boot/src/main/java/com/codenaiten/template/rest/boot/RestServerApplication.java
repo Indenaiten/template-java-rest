cbf1724905bc8a474ee95b21346d64c1c51f84e5
Ángel Herce Soto <indenaiten@gmail.com>
2025-09-04 14:57:51 +0200

    TODOs

--- boot/src/main/java/com/codenaiten/template/rest/boot/RestServerApplication.java
+++ boot/src/main/java/com/codenaiten/template/rest/boot/RestServerApplication.java
@@ -8,6 +8,16 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan( basePackages = { "com.codenaiten.template.rest" })
 public class RestServerApplication {
 
+    // Tests
+    //TODO: 07 - Añadir Tests unitarios al proyecto
+    //TODO: 06 - Añadir Tests de arquitectura al proyecto con ArchJunit
+    //TODO: 05 - Añadir Tests de integración al proyecto
+    //TODO: 04 - Hacer un frontend simple para probar los endpoints (Registro, Login, Logout, Invalidar sesiones, Mostrar mi información y cambiar idioma)
+
+    // Documentación
+    //TODO: 03 - Revisar la documentación OpenAPI de la API Rest para mejorar si es posible
+    //TODO: 02 - Añadir la máxima información al README.md del proyecto
+    //TODO: 01 - Añadir un fichero CHANGELOG.md para listar los cambios del proyecto por versiones
     public static void main( final String[] args ) {
         SpringApplication.run( RestServerApplication.class, args );
     }
